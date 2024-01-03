3d4ff9e387440167df8f7d678c07ab7302ca0cd3
Jaron-S <jaronschoor@gmail.com>
2024-01-03 10:49:21 -0700

    better responsiveness

--- src/components/about/Skills.js
+++ src/components/about/Skills.js
@@ -33,7 +33,7 @@ const Skill = ({ name, x, y, icon, dragConstraints }) => {
   useEffect(() => {
     // Convert animated x, y to pixels
     const xPixel = vwToPixel(x);
-    const yPixel = vhToPixel(y);
+    const yPixel = vwToPixel(y);
 
     // Update constraints to account for animated position
     setConstraints({
@@ -57,7 +57,7 @@ const Skill = ({ name, x, y, icon, dragConstraints }) => {
       >
         <motion.div
           key={JSON.stringify(size)}
-          className="flex flex-center items-center justify-start rounded-full font-semibold bg-dark text-light dark:bg-light dark:text-dark py-3 px-6 shadow-dark cursor-pointer absolute lg:py-2 lg:px-5 md:text-sm"
+          className="flex flex-center items-center justify-start rounded-full font-semibold bg-dark text-light dark:bg-light dark:text-dark py-3 px-6 shadow-dark cursor-pointer absolute lg:py-2 lg:px-5 md:text-sm sm:text-xs"
           whileHover={{ scale: 1.05 }}
           initial={{ x: 0, y: 0 }}
           whileInView={{ x: x, y: y, transition: { duration: 1.5 } }}
@@ -88,11 +88,13 @@ const Skills = () => {
   useEffect(() => {
     if (containerRef.current) {
       const boundingBox = containerRef.current.getBoundingClientRect();
+      const extraXPadding = 35;
+      const extraYPadding = 20;
       setConstraints({
-        top: -boundingBox.height / 2 + 25,
-        left: -boundingBox.width / 2 + 50,
-        right: boundingBox.width / 2 - 50,
-        bottom: boundingBox.height / 2 - 25,
+        top: (-extraYPadding - boundingBox.height) / 2 + 25,
+        left: (-extraXPadding - boundingBox.width) / 2 + 50,
+        right: (extraXPadding + boundingBox.width) / 2 - 50,
+        bottom: (extraYPadding + boundingBox.height) / 2 - 25,
       });
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -102,17 +104,17 @@ const Skills = () => {
     const value = parseFloat(originalValue);
     const unit = originalValue.replace(value, "");
 
-    const minWidth = 0;
-    const maxWidth = 3200;
+    const minWidth = 320;
+    const maxWidth = 2200;
 
     // Clamp width to be within the defined range
     const clampedWidth = Math.min(Math.max(width, minWidth), maxWidth);
 
     // Linearly interpolate the factor based on the width
     const factor =
-      0.5 + (clampedWidth - minWidth) * (1.5 / (maxWidth - minWidth));
+      1 + (clampedWidth - minWidth) * (1.5 / (maxWidth - minWidth));
 
-    return `${value * factor}${unit}`;
+    return `${value / factor}${unit}`;
   };
 
   return (
@@ -144,15 +146,15 @@ const Skills = () => {
             <>
               <Skill
                 name="HTML"
-                x="-35vw"
-                y={adjustPosition("-8vh")}
+                x="-27vw"
+                y={adjustPosition("-20vw")}
                 icon={<HtmlIcon />}
                 dragConstraints={constraints}
               />
               <Skill
                 name="CSS"
-                x="-10vw"
-                y={adjustPosition("-13vh")}
+                x="-35vw"
+                y={adjustPosition("-6vw")}
                 icon={<CssIcon />}
                 dragConstraints={constraints}
               />
@@ -160,70 +162,70 @@ const Skills = () => {
                 name="Javascript"
                 z
                 x="30vw"
-                y={adjustPosition("10vh")}
+                y={adjustPosition("-15vw")}
                 icon={<JsIcon />}
                 dragConstraints={constraints}
               />
               <Skill
                 name="React"
-                x="8vw"
-                y={adjustPosition("-35vh")}
+                x="0vw"
+                y={adjustPosition("-17vw")}
                 icon={<ReactIcon />}
                 dragConstraints={constraints}
               />
               <Skill
                 name="NextJS"
-                x="15vw"
-                y={adjustPosition("18vh")}
+                x="-10vw"
+                y={adjustPosition("-32vw")}
                 icon={<NextJsIcon />}
                 dragConstraints={constraints}
               />
               <Skill
                 name="UI/UX Design"
-                x="-20vw"
-                y={adjustPosition("-25vh")}
+                x="-25vw"
+                y={adjustPosition("25vw")}
                 icon={<UiIcon />}
                 dragConstraints={constraints}
               />
               <Skill
                 name="SQL"
-                x="-20vw"
-                y={adjustPosition("18vh")}
+                x="27vw"
+                y={adjustPosition("24vw")}
                 icon={<SqlIcon />}
                 dragConstraints={constraints}
               />
               <Skill
                 name="Firebase"
-                x="18vw"
-                y={adjustPosition("-27vh")}
+                x="2vw"
+                y={adjustPosition("16vw")}
                 icon={<FirebaseIcon />}
                 dragConstraints={constraints}
               />
               <Skill
                 name="Tailwind CSS"
-                x="24vw"
-                y={adjustPosition("-17vh")}
+                x="-33vw"
+                y={adjustPosition("11vw")}
                 icon={<TailwindIcon />}
                 dragConstraints={constraints}
               />
               <Skill
                 name="Flutter"
-                x="38vw"
-                y={adjustPosition("-7vh")}
+                x="3vw"
+                y={"30vw"}
                 icon={<FlutterIcon />}
                 dragConstraints={constraints}
               />
               <Skill
                 name="Typescript"
-                x="-25vw"
-                y={adjustPosition("10vh")}
+                x="22vw"
+                y={adjustPosition("-30vw")}
                 icon={<TsIcon />}
                 dragConstraints={constraints}
               />
               <Skill
                 name="Express"
-                x="-5vw"
-                y={adjustPosition("26vh")}
+                x="32vw"
+                y={adjustPosition("11vw")}
                 icon={<ExpressIcon />}
                 dragConstraints={constraints}
               />
